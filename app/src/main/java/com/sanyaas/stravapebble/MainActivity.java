0eee0d647eea5f4e0dee81b73463181fefa85705
Subhash S <subhash@boomerangcommerce.com>
2017-10-08 23:11:35 +0530

    first commit

---
+++ app/src/main/java/com/sanyaas/stravapebble/MainActivity.java
@@ -0,0 +1,49 @@
+package com.sanyaas.stravapebble;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    private TextView txtView;
+    private NotificationReceiver nReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtView = (TextView) findViewById(R.id.textView);
+        nReceiver = new NotificationReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.sanyaas.stravapebble.NOTIFICATION_LISTENER");
+        registerReceiver(nReceiver,filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(nReceiver);
+    }
+
+
+
+    public void buttonClicked(View v){
+        Intent i = new Intent("com.sanyaas.stravapebble.NOTIFICATION_LISTENER_SERVICE");
+        i.putExtra("command","list");
+        sendBroadcast(i);
+    }
+
+    class NotificationReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String temp = intent.getStringExtra("notification_event") + "\n" + txtView.getText();
+            txtView.setText(temp);
+        }
+    }
+}
---
+++ app/src/main/java/com/sanyaas/stravapebble/NotificationListener.java
@@ -0,0 +1,132 @@
+package com.sanyaas.stravapebble;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+import com.getpebble.android.kit.Constants;
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
+
+public class NotificationListener extends NotificationListenerService {
+
+    private String TAG = this.getClass().getSimpleName();
+    private NLServiceReceiver nlservicereciver;
+    private Boolean pebbleAppStatus = false;
+    private Integer paceSplitStartTime = 0;
+    private Float paceSplitStartDistance = Float.valueOf(0);
+    private String paceSplitPace = "0:00";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        nlservicereciver = new NLServiceReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.sanyaas.stravapebble.NOTIFICATION_LISTENER_SERVICE");
+        registerReceiver(nlservicereciver,filter);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(nlservicereciver);
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        if (!"com.strava".equalsIgnoreCase(sbn.getPackageName())){
+            return;
+        }
+        Log.i(TAG,"**********  onNotificationPosted");
+        Log.i(TAG,"ID :" + sbn.getId() + "\t" + sbn.getNotification().tickerText + "\t" + sbn.getPackageName());
+        Intent i = new  Intent("com.sanyaas.stravapebble.NOTIFICATION_LISTENER");
+        i.putExtra("notification_event","onNotificationPosted :" + sbn.getPackageName() + "\n" + sbn.getNotification().extras.get("android.title") + "\n");
+        if(!pebbleAppStatus){
+            PebbleKit.startAppOnPebble(getApplicationContext(), Constants.SPORTS_UUID);
+            PebbleDictionary dict = new PebbleDictionary();
+
+            PebbleKit.sendDataToPebble(getApplicationContext(), Constants.SPORTS_UUID, dict);
+            pebbleAppStatus = true;
+        }
+        if ("stop".equalsIgnoreCase(sbn.getNotification().actions[0].title.toString())) {
+            PebbleDictionary dict = new PebbleDictionary();
+
+            String[] text = sbn.getNotification().extras.get("android.title").toString().split(" ");
+
+            // Show a value for duration and distance
+            dict.addString(Constants.SPORTS_TIME_KEY, text[2]);
+            dict.addString(Constants.SPORTS_DISTANCE_KEY, text[4]);
+            dict.addUint8(Constants.SPORTS_LABEL_KEY, (byte)Constants.SPORTS_DATA_PACE);
+            dict.addString(Constants.SPORTS_DATA_KEY, paceMonitor(text[2],text[4]));
+
+            dict.addUint8(Constants.SPORTS_UNITS_KEY, (byte) Constants.SPORTS_UNITS_METRIC);
+
+            PebbleKit.sendDataToPebble(getApplicationContext(), Constants.SPORTS_UUID, dict);
+        }
+        sendBroadcast(i);
+
+    }
+
+    private String paceMonitor (String time, String distance) {
+        Float dist = Float.parseFloat(distance);
+        Float splitDistance = dist - paceSplitStartDistance;
+        if (splitDistance > 0) {
+            String[] timeSplit = time.split(":");
+            Integer currentTime = (Integer.parseInt(timeSplit[0]) * 60) + Integer.parseInt(timeSplit[1]);
+            Integer secondsPerKM = Math.round((currentTime-paceSplitStartTime)/splitDistance);
+            Integer minutesPart = secondsPerKM/60;
+            Integer secondsPart = secondsPerKM%60;
+            String secondsPartString = secondsPart + "";
+            if (secondsPart < 10) {
+                secondsPartString = "0" + secondsPartString;
+            }
+            paceSplitPace = minutesPart + ":" + secondsPartString;
+            paceSplitStartTime = currentTime;
+            paceSplitStartDistance = dist;
+        }
+        return paceSplitPace;
+    }
+
+    @Override
+    public void onNotificationRemoved (StatusBarNotification sbn) {
+        if (!"com.strava".equalsIgnoreCase(sbn.getPackageName())){
+            return;
+        }
+        PebbleKit.closeAppOnPebble(getApplicationContext(), Constants.SPORTS_UUID);
+        pebbleAppStatus = false;
+    }
+
+    class NLServiceReceiver extends BroadcastReceiver{
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Intent i1 = new  Intent("com.sanyaas.stravapebble.NOTIFICATION_LISTENER");
+            i1.putExtra("notification_event","=====================");
+            sendBroadcast(i1);
+            int i=1;
+            for (StatusBarNotification sbn : NotificationListener.this.getActiveNotifications()) {
+                if ("com.strava".equalsIgnoreCase(sbn.getPackageName())){
+                    try {
+                        sbn.getNotification().actions[0].actionIntent.send();
+                    } catch (PendingIntent.CanceledException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Intent i2 = new  Intent("com.sanyaas.stravapebble.NOTIFICATION_LISTENER");
+                i2.putExtra("notification_event",i +" " + sbn.getPackageName() + "\n");
+                sendBroadcast(i2);
+                i++;
+
+            }
+            Intent i3 = new  Intent("com.sanyaas.stravapebble.NOTIFICATION_LISTENER");
+            i3.putExtra("notification_event","===== Notification List ====");
+            sendBroadcast(i3);
+
+        }
+    }
+
+}
